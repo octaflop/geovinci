768e38bef5cc9c461644a64517e6af7da635d34d
Faris Chebib <faris@zaneprep.com>
2015-08-03 11:54:46 -0600

    added geopy and fixed the templates/urls

--- apps/geovinci/settings/base.py
+++ apps/geovinci/settings/base.py
@@ -41,6 +41,7 @@ THIRD_PARTY_APPS = (
 )
 
 LOCAL_APPS = (
+    'geovinci',
     'nearme',
     'world',
 )
--- apps/nearme/urls.py
+++ apps/nearme/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$', 'views.front.index', name='index'),
+    url(r'^$', 'nearme.views.front.index', name='index'),
 ]
