140871ac8d8b6680b8ada563781fb2cf81748f2f
Faris Chebib <faris@zaneprep.com>
2015-08-05 12:01:35 -0600

    updating navbar

--- apps/nearme/views/front.py
+++ apps/nearme/views/front.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     ctx = {}
+    ctx['demo'] = "index"
     template_name = "nearme/front/index.html"
     return render(request, template_name, ctx)
